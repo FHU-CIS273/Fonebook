86f46a70e892725ade029725f695c2706d651d19
Kenan Casey <kenancasey@gmail.com>
2019-11-06 22:31:44 -0600

    Add IFonebook and ListFonebook

---
+++ IFonebook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fonebook
+{
+    public interface IFonebook
+    {
+        IFonebook AddAll(List<Contact> contacts);
+        void Add(Contact contact);
+
+        List<List<Contact>> FindAll(List<string> queries);
+        List<Contact> Find(string query);
+
+        int Size { get; }
+
+    }
+
+
+}
---
+++ ListFonebook.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fonebook
+{
+    public class ListFonebook: IFonebook
+    {
+        private List<Contact> contacts = new List<Contact>();
+
+        public ListFonebook()
+        {
+          
+        }
+
+        public void Add(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public IFonebook AddAll(List<Contact> contacts)
+        {
+            foreach(var c in contacts)
+            {
+                this.Add(c);
+            }
+
+            return this;
+        }
+
+        public List<Contact> Find(string query)
+        {
+            var results = contacts.FindAll( c => c.FirstName.StartsWith(query) || c.LastName.StartsWith(query));
+
+            return results;
+        }
+
+        public List<List<Contact>> FindAll(List<string> queries)
+        {
+            var results = new List<List<Contact>>();
+
+            foreach ( var query in queries)
+            {
+                results.Add(Find(query));
+            }
+
+            return results;
+        }
+
+        public int Size => contacts.Count;
+    }
+}
