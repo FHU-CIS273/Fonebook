9029c500dc5a09239c0a4b28b033f18120e1f459
Kenan Casey <kenancasey@gmail.com>
2019-11-22 11:10:03 -0600

    Add ID queries

--- Generator.cs
+++ Generator.cs
@@ -23,71 +23,101 @@ namespace Fonebook
 
         private static void MakeQuery(Size size)
         {
-            string path;
+            string nameQueryPath;
+            string idQueryPath;
+
             switch (size)
             {
                 case Size.small:
-                    path = "../../../Data/queries_small_100.csv";
+                    nameQueryPath = "../../../Data/queries_small_100.csv";
+                    idQueryPath = "../../../Data/queries_id_small_100.csv";
                     break;
                 case Size.medium:
-                    path = "../../../Data/queries_medium_10000.csv";
+                    nameQueryPath = "../../../Data/queries_medium_10000.csv";
+                    idQueryPath = "../../../Data/queries_id_medium_10000.csv";
                     break;
                 case Size.large:
-                    path = "../../../Data/queries_large_68000.csv";
+                    nameQueryPath = "../../../Data/queries_large_68000.csv";
+                    idQueryPath = "../../../Data/queries_id_large_68000.csv";
                     break;
                 default:
                     throw new System.ArgumentException();
             }
             List<string> first = GetFirstNameList(size);
             List<string> last = GetLastNameList(size);
+            List<int> ids = GetIDList(size);
 
-            using (StreamWriter sw = new StreamWriter(path))
+            using (StreamWriter streamWriter = new StreamWriter(nameQueryPath))
             {
                 foreach(string name in first)
                 {
-                    sw.WriteLine(name);
+                    streamWriter.WriteLine(name);
                 }
                 foreach (string name in last)
                 {
-                    sw.WriteLine(name);
+                    streamWriter.WriteLine(name);
                 }
             }
+
+            using (StreamWriter streamWriter = new StreamWriter(idQueryPath))
+            {
+                foreach (int id in ids)
+                {
+                    streamWriter.WriteLine(id);
+                }
+            }
+
         }
 
         private static void MakeRandomQuery(Size size)
         {
-            string path;
-            int querySize;
+            string nameQueryPath;
+            string idQueryPath;
+
+            int querySize = 0;
             switch (size)
             {
                 case Size.small:
-                    querySize = 50;
-                    path = "../../../Data/queries_small_100.csv";
+                    nameQueryPath = "../../../Data/queries_small_100.csv";
+                    idQueryPath = "../../../Data/queries_id_small_100.csv";
+                    querySize = 100;
                     break;
                 case Size.medium:
-                    querySize = 5000;
-                    path = "../../../Data/queries_medium_10000.csv";
+                    nameQueryPath = "../../../Data/queries_medium_10000.csv";
+                    idQueryPath = "../../../Data/queries_id_medium_10000.csv";
+                    querySize = 10000;
                     break;
                 case Size.large:
-                    querySize = 34000;
-                    path = "../../../Data/queries_large_68000.csv";
+                    nameQueryPath = "../../../Data/queries_large_68000.csv";
+                    idQueryPath = "../../../Data/queries_id_large_68000.csv";
+                    querySize = 68000;
                     break;
                 default:
                     throw new System.ArgumentException();
             }
             List<string> first = GetFirstNameList(size);
             List<string> last = GetLastNameList(size);
+            List<int> ids = GetIDList(size);
             Random random = new Random();
             int index = 0;
 
-            using (StreamWriter sw = new StreamWriter(path))
+            /*using (StreamWriter streamWriter = new StreamWriter(nameQueryPath))
             {
                 for (int i = 0; i < querySize; i++)
                 {
-                    index = random.Next() % querySize;
-                    sw.WriteLine(first[index]);
-                    index = random.Next() % querySize;
-                    sw.WriteLine(last[index]);
+                    index = random.Next() % first.Count;
+                    streamWriter.WriteLine(first[index]);
+                    index = random.Next() % last.Count;
+                    streamWriter.WriteLine(last[index]);
+                }
+            }*/
+
+            using (StreamWriter streamWriter = new StreamWriter(idQueryPath))
+            {
+                for (int i = 0; i < querySize; i++)
+                {
+                    index = random.Next() % ids.Count;
+                    streamWriter.WriteLine(ids[index]);
                 }
             }
         }
@@ -164,6 +194,46 @@ namespace Fonebook
                 {
                     var words = lines[i].Split(",");
                     list.Add(words[2]);
+                }
+
+            }
+            return list;
+        }
+
+        private static List<int> GetIDList(Size size)
+        {
+            int querySize;
+            string path;
+            switch (size)
+            {
+                case Size.small:
+                    querySize = 50;
+                    path = "../../../Data/contacts_small_50.csv";
+                    break;
+                case Size.medium:
+                    querySize = 5000;
+                    path = "../../../Data/contacts_medium_5000.csv";
+                    break;
+                case Size.large:
+                    querySize = 34000;
+                    path = "../../../Data/contacts_large_34000.csv";
+                    break;
+                default:
+                    throw new System.ArgumentException();
+            }
+
+            List<int> list = new List<int>();
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                string text = streamReader.ReadToEnd();
+
+                //Console.WriteLine(text);
+
+                var lines = text.Split(System.Environment.NewLine);
+                for (int i = 1; i < querySize + 1; i++)
+                {
+                    var words = lines[i].Split(",");
+                    list.Add(int.Parse(words[0]));
                 }
 
             }
--- Program.cs
+++ Program.cs
@@ -7,33 +7,43 @@ namespace Fonebook
     {
         static void Main(string[] args)
         {
+
             var contactsSmall = GetContactsFromFile("../../../Data/contacts_small_50.csv");
-            var queriesSmall = GetQueriesFromFile("../../../Data/queries_small_100.csv");
+            var nameQueriesSmall = GetNameQueriesFromFile("../../../Data/queries_small_100.csv");
+            var idQueriesSmall = GetIDQueriesFromFile("../../../Data/queries_id_small_100.csv");
 
             var contactsMedium = GetContactsFromFile("../../../Data/contacts_medium_5000.csv");
-            var queriesMedium = GetQueriesFromFile("../../../Data/queries_medium_10000.csv");
+            var queriesMedium = GetNameQueriesFromFile("../../../Data/queries_medium_10000.csv");
+            var idQueriesMedium = GetIDQueriesFromFile("../../../Data/queries_id_medium_10000.csv");
 
             var contactsLarge = GetContactsFromFile("../../../Data/contacts_large_34000.csv");
-            var queriesLarge = GetQueriesFromFile("../../../Data/queries_large_68000.csv");
+            var queriesLarge = GetNameQueriesFromFile("../../../Data/queries_large_68000.csv");
+            var idQueriesLarge = GetIDQueriesFromFile("../../../Data/queries_id_large_68000.csv");
+
 
             /* List Fonebook */
             IFonebook listFonebook = new ListFonebook();
             CreateFonebook(listFonebook, contactsSmall);
-            QueryFonebook(listFonebook, queriesSmall);
+            QueryFonebook(listFonebook, nameQueriesSmall);
+            QueryFonebook(listFonebook, idQueriesSmall);
 
             listFonebook.Clear();
             CreateFonebook(listFonebook, contactsMedium);
             QueryFonebook(listFonebook, queriesMedium);
+            QueryFonebook(listFonebook, idQueriesMedium);
 
             listFonebook.Clear();
             CreateFonebook(listFonebook, contactsLarge);
             QueryFonebook(listFonebook, queriesLarge);
-
+            QueryFonebook(listFonebook, idQueriesLarge);
 
             /* Dictionary Fonebook */
             IFonebook dictionaryFonebook = new DictionaryFonebook();
             CreateFonebook(dictionaryFonebook, contactsSmall);
-            QueryFonebook(dictionaryFonebook, queriesSmall);
+            QueryFonebook(dictionaryFonebook, nameQueriesSmall);
+            QueryFonebook(dictionaryFonebook, idQueriesSmall);
+
+            // Repeat for medium and large data sets
         }
 
         public static List<Contact> GetContactsFromFile(string fileName)
@@ -41,7 +51,12 @@ namespace Fonebook
             return null;
         }
 
-        public static List<string> GetQueriesFromFile(string fileName)
+        public static List<string> GetNameQueriesFromFile(string fileName)
+        {
+            return null;
+        }
+
+        public static List<int> GetIDQueriesFromFile(string fileName)
         {
             return null;
         }
@@ -62,6 +77,17 @@ namespace Fonebook
             // start stop watch
 
             var results = fonebook.FindAll(names);
+
+            // end stop watch
+
+        }
+
+        public static void QueryFonebook(IFonebook fonebook, List<int> ids)
+        {
+            // Time querying of fonebook
+            // start stop watch
+
+            var results = fonebook.FindAll(ids);
 
             // end stop watch
 
